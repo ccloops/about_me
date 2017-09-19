7d36a52fe40955e321bc82ff4f632a10f415577b
Catherine Looper <loopercat@gmail.com>
2017-09-19 15:38:16 -0700

    Added Question 5 in JS

--- app.js
+++ app.js
@@ -10,45 +10,55 @@ Add, Commit, Push process is being followed; it is evident in GitHub that commit
 // var userName = prompt('Thanks for stopping by my About Me page! What is your name?');
 // alert('Hi, ' + userName + ', nice to meet you. See if you can get the right answers to the following questions about me.');
 
-//Question 1
-var answer1 = prompt('Do I have a fluffy cat?').toLowerCase();
-console.log('Answer 1 is', answer1);
-
-//you must repeat answer1 on both sides or it will result in a truthy problem
-if(answer1 === 'n' || answer1 === 'no') {
-  //Tell the user they got the correct answer
-  alert('No no no no, I am sad to say you are wrong');
-} else {
-  //Tell the user they got the wrong answer
-  alert('Yes, she is fluffy, thanks for noticing');
-}
-
-//Question 2
-var answer2 = prompt('Do I like karaoke?').toLowerCase();
-console.log('Answer 2 is', answer2);
-
-if(answer2 === 'n' || answer2 === 'no') {
-  alert('Are you serious!? Of course I love karaoke!');
-} else {
-  alert('Heck yea!');
-}
-
-//Question 3
-var answer3 = prompt('Do I know how to sail a sailboat?').toLowerCase();
-console.log('Answer 3 is', answer3);
-
-if(answer3 === 'n' || answer3 === 'no') {
-  alert('Surprise! You\'re kinda wrong, I kinda know how ; )');
-} else {
-  alert('You know me so very kinda well, you are kinda right, I kinda know how!')
-}
-
-//Question 4
-var answer4 = prompt('Is my house the color of grass?').toLowerCase();
-console.log('Answer 4 is', answer4);
-
-if(answer4 === 'n' || answer4 === 'no') {
-  alert('You are wrong, although I guess it depends on whether it\'s summer or not');
-} else {
-  alert('yea, you got it');
-}
+// //Question 1
+// var answer1 = prompt('Do I have a fluffy cat?').toLowerCase();
+// console.log('Answer 1 is', answer1);
+//
+// //you must repeat answer1 on both sides or it will result in a truthy problem
+// if(answer1 === 'n' || answer1 === 'no') {
+//   //Tell the user they got the correct answer
+//   alert('No no no no, I am sad to say you are wrong');
+// } else {
+//   //Tell the user they got the wrong answer
+//   alert('Yes, she is fluffy, thanks for noticing');
+// }
+//
+// //Question 2
+// var answer2 = prompt('Do I like karaoke?').toLowerCase();
+// console.log('Answer 2 is', answer2);
+//
+// if(answer2 === 'n' || answer2 === 'no') {
+//   alert('Are you serious!? Of course I love karaoke!');
+// } else {
+//   alert('Heck yea!');
+// }
+//
+// //Question 3
+// var answer3 = prompt('Do I know how to sail a sailboat?').toLowerCase();
+// console.log('Answer 3 is', answer3);
+//
+// if(answer3 === 'n' || answer3 === 'no') {
+//   alert('Surprise! You\'re kinda wrong, I kinda know how ; )');
+// } else {
+//   alert('You know me so very kinda well, you are kinda right, I kinda know how!')
+// }
+//
+// //Question 4
+// var answer4 = prompt('Is my house the color of grass?').toLowerCase();
+// console.log('Answer 4 is', answer4);
+//
+// if(answer4 === 'n' || answer4 === 'no') {
+//   alert('You are wrong, although I guess it depends on whether it\'s summer or not');
+// } else {
+//   alert('yea, you got it');
+// }
+//
+// //Question 5
+// var answer5 = prompt('Do I like the rain?').toLowerCase();
+// console.log('Answer 5 is', answer5);
+//
+// if(answer5 === 'n' || answer5 === 'no') {
+//   alert('Hey now! I have to like it at least a little right?');
+// } else {
+//   alert('yeaaaaa, yea I do : )');
+// }
