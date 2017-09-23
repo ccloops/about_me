b6c7109c9f57c76dc5d6c3af23d682ef523bec63
Catherine Looper <loopercat@gmail.com>
2017-09-22 17:25:55 -0700

    Added Stretch Goal Number 1 for About Me Project

--- app.js
+++ app.js
@@ -3,80 +3,114 @@
 var score = 0;
 
   //Class Example Question
-  var userName = prompt('Hello!!! Welcome to me! Thanks for coming. What is your name?');
-  alert('Hi, ' + userName + ', nice to meet you. See if you can get the right answers to the following questions about me.');
-  console.log('User Name is', userName);
+var userName = prompt('Hello!!! Welcome to me! Thanks for coming. What is your name?');
+alert('Hi, ' + userName + ', nice to meet you. See if you can get the right answers to the following questions about me.');
+console.log('User Name is', userName);
 
-//Question 1
-function question1() {
-  var answer1 = prompt('Do I have a fluffy cat?').toLowerCase();
-  console.log('Do I have a fluffy cat? User response to Answer 1:', answer1);
+var questionsArray = ['Do I have a fluffy cat?',
+  'Do I hate karaoke?',
+  'Do I know how to sail a sailboat?',
+  'Is my house the color of grass?',
+  'Do I like the rain?'];
+var correctAnswerArray = ['y', 'n', 'y', 'y', 'y'];
+var correctResponseArray = ['Yes! She moosh, she floof, she\'s very nice',
+  'Yea you got it right! I love karaoke!',
+  'You know me so very kinda well, you are kinda right, I kinda know how!',
+  'Yea, weirdo my house is green',
+  'Yeaaaaa, yea I do : )'];
+var wrongResponseArray = ['No no no no, I am sad to say you are wrong. I definitely have a fluffy cat!',
+  'Awww, no I don\'t hate karaoke, it\'s the best!',
+  'Surprise! You\'re kinda wrong, I kinda know how ; )',
+  'Trick question! Depends on whether or not it\'s been raining! But basically you got the answer wrong.',
+  'Hey now! I have to like it at least a little right?'];
 
-  //you must repeat answer1 on both sides or it will result in a truthy problem
-  if(answer1 === 'y' || answer1 === 'yes') {
-    alert('Yes! She moosh, she floof, she\'s very nice');
+for(var i = 0; i < questionsArray.length; i++) {
+  var a = prompt(questionsArray[i]).toLowerCase();
+  if(a === ('yes' || 'y')) {
+    a = 'y';
+  } else if(a === ('no' || 'n')) {
+    a = 'n';
+  } else {
+    alert('Invalid Input');
+  }
+  if(a === correctAnswerArray[i]) {
+    alert(correctResponseArray[i]);
     score++;
   } else {
-    alert('No no no no, I am sad to say you are wrong. I definitely have a fluffy cat!');
+    alert(wrongResponseArray[i]);
   }
 }
-question1();
 
-//Question 2
-function question2() {
-  var answer2 = prompt('Do I hate karaoke?').toLowerCase();
-  console.log('Do I hate karaoke? User response to Answer 2:', answer2);
-
-  if(answer2 === 'n' || answer2 === 'no') {
-    alert('Yea you got it right! I love karaoke!');
-    score++;
-  } else {
-    alert('Awww, no I don\'t hate karaoke, it\'s the best!');
-  }
-}
-question2();
-
-//Question 3
-function question3(){
-  var answer3 = prompt('Do I know how to sail a sailboat?').toLowerCase();
-  console.log('Do I know how to sail a sailboat? User response to Answer 3:', answer3);
-
-  if(answer3 === 'y' || answer3 === 'yes') {
-    alert('You know me so very kinda well, you are kinda right, I kinda know how!');
-    score++;
-  } else {
-    alert('Surprise! You\'re kinda wrong, I kinda know how ; )');
-  }
-}
-question3();
-
-//Question 4
-function question4() {
-  var answer4 = prompt('Is my house the color of grass?').toLowerCase();
-  console.log('Is my house the color of grass? User response to Answer 4:', answer4);
-
-  if(answer4 === 'y' || answer4 === 'yes') {
-    alert('Yea, weirdo my house is green');
-    score++;
-  } else {
-    alert('Trick question! Depends on whether or not it\'s been raining! But basically you got the answer wrong.');
-  }
-}
-question4();
-
-//Question 5
-function question5() {
-  var answer5 = prompt('Do I like the rain?').toLowerCase();
-  console.log('Do I like the rain? User response to Answer 5:', answer5);
-
-  if(answer5 === 'y' || answer5 === 'yes') {
-    alert('Yeaaaaa, yea I do : )');
-    score++;
-  } else {
-    alert('Hey now! I have to like it at least a little right?');
-  }
-}
-question5();
+// //Question 1
+// function question1() {
+//   var answer1 = prompt('Do I have a fluffy cat?').toLowerCase();
+//   console.log('Do I have a fluffy cat? User response to Answer 1:', answer1);
+//
+//   //you must repeat answer1 on both sides or it will result in a truthy problem
+//   if(answer1 === 'y' || answer1 === 'yes') {
+//     alert('Yes! She moosh, she floof, she\'s very nice');
+//     score++;
+//   } else {
+//     alert('No no no no, I am sad to say you are wrong. I definitely have a fluffy cat!');
+//   }
+// }
+// question1();
+//
+// //Question 2
+// function question2() {
+//   var answer2 = prompt('Do I hate karaoke?').toLowerCase();
+//   console.log('Do I hate karaoke? User response to Answer 2:', answer2);
+//
+//   if(answer2 === 'n' || answer2 === 'no') {
+//     alert('Yea you got it right! I love karaoke!');
+//     score++;
+//   } else {
+//     alert('Awww, no I don\'t hate karaoke, it\'s the best!');
+//   }
+// }
+// question2();
+//
+// //Question 3
+// function question3(){
+//   var answer3 = prompt('Do I know how to sail a sailboat?').toLowerCase();
+//   console.log('Do I know how to sail a sailboat? User response to Answer 3:', answer3);
+//
+//   if(answer3 === 'y' || answer3 === 'yes') {
+//     alert('You know me so very kinda well, you are kinda right, I kinda know how!');
+//     score++;
+//   } else {
+//     alert('Surprise! You\'re kinda wrong, I kinda know how ; )');
+//   }
+// }
+// question3();
+//
+// //Question 4
+// function question4() {
+//   var answer4 = prompt('Is my house the color of grass?').toLowerCase();
+//   console.log('Is my house the color of grass? User response to Answer 4:', answer4);
+//
+//   if(answer4 === 'y' || answer4 === 'yes') {
+//     alert('Yea, weirdo my house is green');
+//     score++;
+//   } else {
+//     alert('Trick question! Depends on whether or not it\'s been raining! But basically you got the answer wrong.');
+//   }
+// }
+// question4();
+//
+// //Question 5
+// function question5() {
+//   var answer5 = prompt('Do I like the rain?').toLowerCase();
+//   console.log('Do I like the rain? User response to Answer 5:', answer5);
+//
+//   if(answer5 === 'y' || answer5 === 'yes') {
+//     alert('Yeaaaaa, yea I do : )');
+//     score++;
+//   } else {
+//     alert('Hey now! I have to like it at least a little right?');
+//   }
+// }
+// question5();
 
 //Question 6
 function question6() {
